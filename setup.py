8002643ce5c484fd5cb13071eafe86141d6757da
Jesus Anaya <jesus.anaya.dev@gmail.com>
2014-12-22 01:24:29 -0800

    Updated setup.py installer

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='Django CRM',
+    version='0.0.1',
+    description='Django CRM Framework',
+    author='Jesus Anaya',
+    author_email='jesus.anaya.dev@gmail.com',
+    url='https://github.com/JesusAnaya/django-crm/',
+    packages=['django_crm'],
+)
